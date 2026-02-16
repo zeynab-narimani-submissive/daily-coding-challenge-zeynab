68368b31def60729b986a5af2a1b5da78b01d214
zeynab-narimani-submissive <lari.narimani.zeinab@gmail.com>
2026-02-17 03:45:19 +1100

    first day

---
+++ day1.py
@@ -0,0 +1,10 @@
+def find_max(numbers):
+    if not numbers:
+        return None
+    max_num = numbers[0]
+    for num in numbers[1:]:
+        if num > max_num:
+            max_num = num
+    return max_num
+
+print(find_max([3,1,4,1,5,9,2,6]))
