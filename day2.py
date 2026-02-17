f73a42d0975e9cf274930b4b4c5cac0c889b95d8
zeynab narimani <lari.narimani.zeinab@gmail.com>
2026-02-18 02:55:22 +1100

    new:palindrome checker function in Python

---
+++ day2.py
@@ -0,0 +1,17 @@
+from operator import truediv
+
+
+def is_palindrome(s):
+    cleaned = ''.join(c.lower() for c in s if c.isalnum())
+    reversed = ""
+    neg = int("-" + str(len(cleaned)))
+    i = -1
+    while i >= neg:
+        reversed += cleaned[i]
+        i -= 1
+    if s == reversed:
+        return True
+    else:
+        return False
+
+print(is_palindrome("lev"))
